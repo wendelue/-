1abc1d37ea5883330d04cf0b6eed0c0e16a1dfe7
wendelue <1181441146@qq.com>
2018-12-10 16:36:28 +0800

    process

--- proccess_data.py
+++ proccess_data.py
@@ -4,4 +4,24 @@
 
 import numpy as np
 
- 
+from read_data import dataset
+
+def process(data):
+    length = len(data)
+    batch_img = np.ones((length, 15, 4, 101, 101))
+    label_img = np.ones((length,1))
+    for i in range(length):
+        info = data[i]
+        batch_img[i] = np.reshape(info[1], [15, 4, 101, 101])
+        label_img[i] = info[2]
+        print(info[0] + 'has processed!')
+    print('end!')
+    return batch_img,label_img
+
+
+
+if __name__ == '__main__':
+    data = dataset('../dataset')
+    train_data = data.data_generateor('training')
+    batch_img, label_img = process(next(train_data))
+    print(np.shape(batch_img), label_img)
