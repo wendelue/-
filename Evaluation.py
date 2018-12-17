de67ccbee74153d4d058d6fcb58eac2e054b9fbf
wendelue <1181441146@qq.com>
2018-12-17 18:26:56 +0800

    12.17

--- Evaluation.py
+++ Evaluation.py
@@ -3,9 +3,23 @@
 
 import scipy.io
 import pandas as pd
-import numpy as numpy
+from utils import mean_absolute_error,mean_squared_error,root_mean_squared_error,r2_score
+
 
 #读取数据
-
+pred = pd.read_csv("saved_results/model_conv2d/predict.csv")
+real = pd.read_csv("saved_results/model_conv2d/real_data.csv")
+pred = pred.values
+real = real.values
 
 #计算指标
+mae = mean_absolute_error(real, pred)
+mse = mean_squared_error(real, pred)
+rmse = root_mean_squared_error(real, pred)
+r2 = r2_score(real,pred)
+
+
+print('mae:', mae)
+print('mse:', mse)
+print('rmse:', rmse)
+print('r2_score:',r2)
--- utils.py
+++ utils.py
@@ -4,15 +4,14 @@
 import scipy.io
 import matplotlib.pyplot as plt
 import pandas as pd
-
+import numpy as np
+from math import sqrt
 
 def save_data(file_dir,filename,data):
     #scipy.io.savemat(file_dir + filename + '.mat', {filename: data})
     save = pd.DataFrame(data, columns=[filename])
     save.to_csv(file_dir + filename + '.csv',index=False)
 
-def load_data():
-    pass
 
 def plot_fig(data1,data2,label1,label2):
     plt.plot(data1,label=label1)
@@ -21,11 +20,30 @@ def plot_fig(data1,data2,label1,label2):
     plt.show()
 
 
-#计算MSE/R^2
-def score(real_y, pred_y):
-    pass
+
+def mean_squared_error(y_true, y_predict):
+    """计算y_true和y_predict之间的MSE"""
+    assert len(y_true) == len(y_predict), \
+        "the size of y_true must be equal to the size of y_predict"
  
-
-
-if __name__ == "__main__":
-    pass
+    return np.sum((y_true - y_predict)**2) / len(y_true)
+ 
+ 
+def root_mean_squared_error(y_true, y_predict):
+    """计算y_true和y_predict之间的RMSE"""
+ 
+    return sqrt(mean_squared_error(y_true, y_predict))
+ 
+ 
+def mean_absolute_error(y_true, y_predict):
+    """计算y_true和y_predict之间的MAE"""
+    assert len(y_true) == len(y_predict), \
+        "the size of y_true must be equal to the size of y_predict"
+ 
+    return np.sum(np.absolute(y_true - y_predict)) / len(y_true)
+ 
+ 
+def r2_score(y_true, y_predict):
+    """计算y_true和y_predict之间的R Square"""
+ 
+    return 1 - mean_squared_error(y_true, y_predict)/np.var(y_true)
